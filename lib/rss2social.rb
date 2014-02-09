88d799a0b02bea446c6c9a13725b92e6c400d301
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2014-02-09 16:31:57 +0900

    completion fetche and update rss_entries table

--- lib/rss2social.rb
+++ lib/rss2social.rb
@@ -1,5 +1,6 @@
 require 'rss'
 require 'yaml'
+require 'sqlite3'
 require 'singleton'
 
 module RSS2Social
@@ -7,28 +8,40 @@ module RSS2Social
     def initialize
       @src = Source.instance
       @dest = Destination.instance
-      @model = RSSEntry.new
-      @controller = RSSController.new(@src)
+      @model = RSSEntryModel.new
+      @controller = RSSController.new(@src, @model)
     end
-
+    
     def run
       @controller.update
     end
   end
 
   class RSSController
-    def initialize(src)
+    def initialize(src, model)
       @src = src
+      @model = model
     end
 
     def update
       entries = RSS::Parser.parse(@src.list[0]['url'])
-      puts entries
+      entries.items.each do |item|
+        @model.insert(item.title, item.link) unless @model.select(item.title, item.link)
+      end
     end
   end
 
-  class RSSEntry
+  class RSSEntryModel
     def initialize
+      @db = SQLite3::Database.new(File.expand_path('../../db', __FILE__) + '/rss.db')
+    end
+
+    def insert(title, url)
+      @db.execute("INSERT INTO rss_entries(title, url) VALUES (?, ?)", [title, url])
+    end
+
+    def select(title, url)
+      @db.execute("SELECT title, url FROM rss_entries WHERE title=? AND url=?", [title, url])
     end
   end
 
